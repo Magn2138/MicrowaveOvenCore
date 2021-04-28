c0f30561604f194e6d58a4074ada49879114c39c
Lasseerenged <201806450@post.au.dk>
2021-04-28 09:11:16 +0200

    Tilføjet NSubstitute

--- Microwave.Test.Integration/IT1_CookControllerTimerPowerTubeDisplay.cs
+++ Microwave.Test.Integration/IT1_CookControllerTimerPowerTubeDisplay.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NSubstitute;
 using NUnit.Framework;
+using Microwave.Classes.Interfaces;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Boundary;
 
 namespace Microwave.Test.Integration
 {
     class IT1_CookControllerTimerPowerTubeDisplay
     {
+        private IUserInterface _userInterface;
+        private IOutput _output;
+        private CookController _cookController;
+        private Timer _timer;
+        private Display _display;
+        private PowerTube _powerTube;
+
+
         [SetUp]
         public void Setup()
         {
-
+            
         }
 
         [Test]
