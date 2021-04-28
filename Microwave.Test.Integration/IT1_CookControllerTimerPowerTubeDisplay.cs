4eb70bc67ef74090fd746a915214a9bd68ac05e0
yuhu0006 <yuhu0006@gmail.com>
2021-04-28 08:48:35 +0200

    Updated every class in intergrationtest

---
+++ Microwave.Test.Integration/IT1_CookControllerTimerPowerTubeDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class IT1_CookControllerTimerPowerTubeDisplay
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+	}
+}
---
+++ Microwave.Test.Integration/IT2_UserInterfaceDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class IT2_UserInterfaceDisplay
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
