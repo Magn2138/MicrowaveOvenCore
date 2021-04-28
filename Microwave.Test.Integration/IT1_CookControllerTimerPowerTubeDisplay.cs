d816c284100ae3030776b455d61354ee09f0d0e6
Lasseerenged <201806450@post.au.dk>
2021-04-28 10:12:21 +0200

    IT1 påbegyndt

--- Microwave.Test.Integration/IT1_CookControllerTimerPowerTubeDisplay.cs
+++ Microwave.Test.Integration/IT1_CookControllerTimerPowerTubeDisplay.cs
@@ -13,22 +13,37 @@ namespace Microwave.Test.Integration
     {
         private IUserInterface _userInterface;
         private IOutput _output;
-        private CookController _cookController;
         private Timer _timer;
         private Display _display;
         private PowerTube _powerTube;
+        private CookController _cookController;
+        
 
 
         [SetUp]
         public void Setup()
         {
-            
+            _userInterface = Substitute.For<IUserInterface>();
+            _output = Substitute.For<IOutput>();
+            _timer = new Timer();
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
+            _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
         }
 
         [Test]
-        public void Test1()
+        public void StartCooking_PowerSetTo10_PowerTubeLogsCorrectly()
         {
+            _cookController.StartCooking(10,10);
+            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("PowerTube works with 10")));
+        }
+
+        [Test]
+        public void StartCooking_TimerSetTo10_DisplayLogsNewTimeAfterTick()
+        {
+            //_cookController.StartCooking(10, 10);
+            //_output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("Display shows: 00:09")));
             Assert.Pass();
         }
-	}
+    }
 }
